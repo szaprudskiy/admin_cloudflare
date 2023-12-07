eaf42fde113fc62f0c7cea13c802aed8c6f917e4
szaprudskiy <sergei.zaprudskiy@gmail.com>
2023-12-07 19:11:22 +0200

    updateIP

--- src/components/record/Record.tsx
+++ src/components/record/Record.tsx
@@ -1,6 +1,78 @@
+import React, { useState } from 'react'
+import { ToastContainer, toast } from 'react-toastify'
 import styles from './Record.module.css'
+import axios from 'axios'
 
-const Record = () => {
-  return <></>
+import 'react-toastify/dist/ReactToastify.css'
+
+const Record = (props: any) => {
+  const [ip, setIp] = useState<string>('')
+  console.log('props', props)
+  const records = props.records
+
+  const firstRecord = records[0]
+
+  const handleUpdate = async (e: React.FormEvent) => {
+    e.preventDefault()
+    const updateRecord = axios.post(
+      'http://localhost:4002/api/updaterecord',
+      {
+        content: ip,
+        name: firstRecord.name,
+        type: firstRecord.type,
+        zoneId: firstRecord.zone_id,
+        id: firstRecord.id,
+        key: props.accountkey,
+      },
+      {
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      }
+    )
+    console.log('updateRecord', updateRecord)
+    toast.success('IP успешно обновился', {
+      position: 'top-right',
+      autoClose: 5000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+      theme: 'colored',
+    })
+    try {
+    } catch (error) {
+      console.error('Weng wrong update record type', error)
+    }
+  }
+
+  return (
+    <div>
+      <ToastContainer />
+      {firstRecord && (
+        <form
+          className="p-4 border mb-4"
+          onSubmit={handleUpdate}
+          key={firstRecord.id}
+        >
+          <h1 className="text-lg font-semibold mb-2">Запись</h1>
+          <div className="mb-2">{firstRecord.name}</div>
+          <div className="mb-2">{firstRecord.content}</div>
+          <div className="mb-2">{firstRecord.type}</div>
+          <input
+            type="text"
+            placeholder="IP"
+            value={ip}
+            onChange={(e) => setIp(e.target.value)}
+            className="p-2 border mb-2 rounded"
+          />
+          <button className="bg-green-500 text-white p-2 rounded cursor-pointer">
+            Обновить IP записи
+          </button>
+        </form>
+      )}
+    </div>
+  )
 }
 export default Record
--- src/components/records/Records.tsx
+++ src/components/records/Records.tsx
@@ -1,6 +1,7 @@
 import { useState, FormEvent, useEffect } from 'react'
 import axios from 'axios'
 import { FormData, Account } from '../interfaces/account'
+import Record from '../record/Record'
 import styles from './Records.module.css'
 
 const Records: React.FC = () => {
@@ -8,12 +9,9 @@ const Records: React.FC = () => {
   const [accountKey, setAccountKey] = useState<string>('')
   const [domainName, setDomainName] = useState<string>('')
   const [domainId, setDomainId] = useState<string>('')
-
-  const [key, setKey] = useState<string>('')
   const [records, setRecords] = useState<[]>([])
-  const [zone, setZone] = useState<string>('')
   const [accounts, setAccounts] = useState<Account[]>([])
-  const [selectedAccount, setSelectedaAccout] = useState<string>('')
+  const [selectedAccount, setSelectedAccount] = useState<string>('')
   const [selectedDomain, setSelectedDomain] = useState<string>('')
 
   const handleSubmit = async (e: React.FormEvent) => {
@@ -82,82 +80,107 @@ const Records: React.FC = () => {
   }
 
   return (
-    <div className={styles.records}>
-      <form onSubmit={handleSubmit}>
-        <input
-          className={styles.input}
-          type="text"
-          placeholder="Укажите название аккаунта"
-          value={accountName}
-          onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
-            setAccountName(e.target.value)
-          }
-        />
-        <input
-          className={styles.input}
-          type="text"
-          placeholder="Укажите ключ аккаунта"
-          value={accountKey}
-          onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
-            setAccountKey(e.target.value)
-          }
-        />
-        <input
-          className={styles.input}
-          type="text"
-          placeholder="Укажите название домена"
-          value={domainName}
-          onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
-            setDomainName(e.target.value)
-          }
-        />
-        <input
-          className={styles.input}
-          type="text"
-          placeholder="Укажите айди домена"
-          value={domainId}
-          onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
-            setDomainId(e.target.value)
-          }
-        />
-        <button className={styles.button} type="submit">
+    <div className="container mx-auto p-8 bg-gray-100 rounded shadow">
+      <form onSubmit={handleSubmit} className="mb-6">
+        <h1 className="text-3xl font-semibold mb-4">Добавить аккаунт</h1>
+        <div className="grid grid-cols-2 gap-4">
+          <div>
+            <input
+              className="p-2 border mb-2 rounded w-8/12"
+              type="text"
+              placeholder="Укажите название аккаунта"
+              value={accountName}
+              onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                setAccountName(e.target.value)
+              }
+            />
+          </div>
+          <div>
+            <input
+              className="p-2 border mb-2 rounded w-8/12"
+              type="text"
+              placeholder="Укажите ключ аккаунта"
+              value={accountKey}
+              onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                setAccountKey(e.target.value)
+              }
+            />
+          </div>
+        </div>
+        <div className="grid grid-cols-2 gap-4 mt-4">
+          <div>
+            <input
+              className="p-2 border mb-2 rounded w-8/12"
+              type="text"
+              placeholder="Укажите название домена"
+              value={domainName}
+              onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                setDomainName(e.target.value)
+              }
+            />
+          </div>
+          <div>
+            <input
+              className="p-2 border mb-2 rounded w-8/12"
+              type="text"
+              placeholder="Укажите айди домена"
+              value={domainId}
+              onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                setDomainId(e.target.value)
+              }
+            />
+          </div>
+        </div>
+        <button
+          className="button bg-blue-500 text-white p-2 rounded cursor-pointer"
+          type="submit"
+        >
           Добавить аккаунт
         </button>
       </form>
-      <form onSubmit={handleGetRecords}>
-        <select
-          className={styles.selectzone}
-          defaultValue="default"
-          onChange={(e) => setSelectedaAccout(e.target.value)}
-          value={selectedAccount}
-        >
-          <option value="default" disabled hidden>
-            Choose an account
-          </option>
-          {accounts.map((item, index) => (
-            <option key={index} value={item.account.accountKey}>
-              {item.account.accountName}
-            </option>
-          ))}
-        </select>
-        <select
-          className={styles.selectzone}
-          defaultValue="default"
-          onChange={(e) => setSelectedDomain(e.target.value)}
-          value={selectedDomain}
-        >
-          <option value="default" disabled hidden>
-            Choose an domain
-          </option>
-          {accounts.map((item, index) => (
-            <option key={index} value={item.domain.domainId}>
-              {item.domain.domainName}
-            </option>
-          ))}
-        </select>
-
-        <button className={styles.button}>Получить данные</button>
+      <form onSubmit={handleGetRecords} className="mb-6">
+        <h1 className="text-3xl font-semibold mb-4">Получить данные</h1>
+        <div className="grid grid-cols-2 gap-4">
+          <div>
+            <label className="text-sm text-gray-600 block mb-1">
+              Выберите аккаунт
+            </label>
+            <select
+              className="select"
+              onChange={(e) => setSelectedAccount(e.target.value)}
+              value={selectedAccount}
+            >
+              <option value="default">Выберите аккаунт</option>
+              {accounts.map((item, index) => (
+                <option key={index} value={item.account.accountKey}>
+                  {item.account.accountName}
+                </option>
+              ))}
+            </select>
+          </div>
+          <div>
+            <label className="text-sm text-gray-600 block mb-1">
+              Выберите домен
+            </label>
+            <select
+              className="select"
+              onChange={(e) => setSelectedDomain(e.target.value)}
+              value={selectedDomain}
+            >
+              <option value="default">Выберите домен</option>
+              {accounts.map((item, index) => (
+                <option key={index} value={item.domain.domainId}>
+                  {item.domain.domainName}
+                </option>
+              ))}
+            </select>
+          </div>
+        </div>
+        <button className="bg-green-500 text-white p-2 rounded cursor-pointer">
+          Получить данные
+        </button>
       </form>
+      <Record records={records} accountkey={selectedAccount} />
     </div>
   )
 }
